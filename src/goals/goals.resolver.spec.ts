0d10c1006d7d7d7142486a674a86f2cac9ee4144
Maria Lucia Solero <marialucia.solero@gmail.com>
2023-08-16 03:44:12 +0100

    feat: rename operations

--- src/goals/goals.resolver.spec.ts
+++ src/goals/goals.resolver.spec.ts
@@ -1,117 +1,121 @@
-import { Test, TestingModule } from "@nestjs/testing";
-import { GoalsResolver } from "./goals.resolver";
-import { GoalsService } from "./goals.service";
-import { Prisma } from "@prisma/client";
-import { Goal } from "./models/goal.model";
-
+import { Test, TestingModule } from '@nestjs/testing';
+import { GoalsResolver } from './goals.resolver';
+import { GoalsService } from './goals.service';
+import { Prisma } from '@prisma/client';
+import { Goal } from './models/goal.model';
 
 describe('GoalsResolver', () => {
-    let resolver: GoalsResolver;
+  let resolver: GoalsResolver;
 
-    beforeEach(async () => {
-        const module: TestingModule = await Test.createTestingModule({
-            providers: [
-                GoalsResolver,
-                {
-                    provide: GoalsService,
-                    useFactory: () => ({
-                        createGoal: jest.fn((data: Prisma.GoalCreateInput) => ({
-                            id: 123,
-                            ...data,
-                        })),
-                        findGoals: jest.fn(() => [
-                            {
-                                id: 1, 
-                                text: 'First Goal',
-                                completed: false,
-                            },
-                            {
-                                id: 2,
-                                text: 'Second Goal',
-                                completed: true,
-                            }
-                        ]),
-                        findGoal: jest.fn((where: Prisma.GoalWhereUniqueInput) => ({
-                            id: where.id,
-                            text: 'First Goal',
-                            completed: false
-                        })),
-                        editGoal: jest.fn((params: {where: Prisma.GoalWhereUniqueInput, data: Prisma.GoalUpdateInput}) => ({
-                            id: params.where.id,
-                            ...params.data
-                        })),
-                        deleteGoal: jest.fn((where: Prisma.GoalWhereUniqueInput) => ({
-                            id: where.id,
-                            text: 'Goal Deleted',
-                            completed: false
-                        }))
-                    })
-                }
-            ]
-        }).compile()
-
-        resolver = module.get<GoalsResolver>(GoalsResolver);
-    })
-
-    it('should exist', () => {
-        expect(resolver).toBeDefined();
-    })
-
-
-    it('should return array of goals', async() => {
-        let test = await resolver.findGoals();
-        expect(test).toEqual([
-            {
-                id: 1, 
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        GoalsResolver,
+        {
+          provide: GoalsService,
+          useFactory: () => ({
+            createGoal: jest.fn((data: Prisma.GoalCreateInput) => ({
+              id: 123,
+              ...data,
+            })),
+            findGoals: jest.fn(() => [
+              {
+                id: 1,
                 text: 'First Goal',
                 completed: false,
-            },
-            {
+              },
+              {
                 id: 2,
                 text: 'Second Goal',
                 completed: true,
-            }
-        ])
+              },
+            ]),
+            findGoal: jest.fn((where: Prisma.GoalWhereUniqueInput) => ({
+              id: where.id,
+              text: 'First Goal',
+              completed: false,
+            })),
+            editGoal: jest.fn(
+              (params: {
+                where: Prisma.GoalWhereUniqueInput;
+                data: Prisma.GoalUpdateInput;
+              }) => ({
+                id: params.where.id,
+                ...params.data,
+              }),
+            ),
+            deleteGoal: jest.fn((where: Prisma.GoalWhereUniqueInput) => ({
+              id: where.id,
+              text: 'Goal Deleted',
+              completed: false,
+            })),
+          }),
+        },
+      ],
+    }).compile();
 
-    })
+    resolver = module.get<GoalsResolver>(GoalsResolver);
+  });
 
-    it('should return one goal with the given id', async() => {
-        let test = await resolver.findGoal(1);
-        expect(test).toEqual(
-            {
-                id: 1, 
-                text: 'First Goal',
-                completed: false,
-            }
-        )
-    })
+  it('should exist', () => {
+    expect(resolver).toBeDefined();
+  });
 
-    it('should update one existing goal with the given input', async() => {
-        let testGoal: Goal = {
-            id: 1,
-            text: 'Edited Goal',
-            completed: false,
-        }
-        let test = await resolver.editGoal(testGoal.id, testGoal.text, testGoal.completed);
-        expect(test).toEqual({ ...testGoal });
-    })
+  it('should return array of goals', async () => {
+    let test = await resolver.goals();
+    expect(test).toEqual([
+      {
+        id: 1,
+        text: 'First Goal',
+        completed: false,
+      },
+      {
+        id: 2,
+        text: 'Second Goal',
+        completed: true,
+      },
+    ]);
+  });
 
-    it('should create one goal with the given input', async() => {
-        let testGoal: Goal = {
-            id: 123,
-            text: 'Created Goal',
-            completed: false,
-        }
-        let test = await resolver.newGoal(testGoal.text, testGoal.completed);
-        expect(test).toEqual({ ...testGoal });
-    })
+  it('should return one goal with the given id', async () => {
+    let test = await resolver.goal(1);
+    expect(test).toEqual({
+      id: 1,
+      text: 'First Goal',
+      completed: false,
+    });
+  });
 
-    it('should remove one goal with the given id', async() => {
-        let test = await resolver.deleteGoal(123);
-        expect(test).toEqual({
-            id: 123,
-            text: 'Goal Deleted',
-            completed: false
-        })
-    })
-})
+  it('should update one existing goal with the given input', async () => {
+    let testGoal: Goal = {
+      id: 1,
+      text: 'Edited Goal',
+      completed: false,
+    };
+    let test = await resolver.editGoal(
+      testGoal.id,
+      testGoal.text,
+      testGoal.completed,
+    );
+    expect(test).toEqual({ ...testGoal });
+  });
+
+  it('should create one goal with the given input', async () => {
+    let testGoal: Goal = {
+      id: 123,
+      text: 'Created Goal',
+      completed: false,
+    };
+    let test = await resolver.newGoal(testGoal.text, testGoal.completed);
+    expect(test).toEqual({ ...testGoal });
+  });
+
+  it('should remove one goal with the given id', async () => {
+    let test = await resolver.deleteGoal(123);
+    expect(test).toEqual({
+      id: 123,
+      text: 'Goal Deleted',
+      completed: false,
+    });
+  });
+});
--- src/goals/goals.resolver.ts
+++ src/goals/goals.resolver.ts
@@ -1,61 +1,67 @@
-import { ConflictException, NotFoundException } from "@nestjs/common";
-import { Args, Resolver, Query, Mutation } from "@nestjs/graphql";
-import { GoalsService } from "./goals.service";
-import { Goal } from "./models/goal.model";
+import { ConflictException, NotFoundException } from '@nestjs/common';
+import { Args, Resolver, Query, Mutation } from '@nestjs/graphql';
+import { GoalsService } from './goals.service';
+import { Goal } from './models/goal.model';
 
-@Resolver(of => Goal)
+@Resolver((of) => Goal)
 export class GoalsResolver {
-    constructor( private readonly goalsService: GoalsService ) {}
+  constructor(private readonly goalsService: GoalsService) {}
 
-    @Query(returns => [Goal])
-    async findGoals(): Promise<Goal[]> {
-        return this.goalsService.findGoals();
+  @Query((returns) => [Goal])
+  async goals(): Promise<Goal[]> {
+    return this.goalsService.findGoals();
+  }
+
+  @Query((returns) => Goal)
+  async goal(@Args('id') id: number): Promise<Goal> {
+    const goal = await this.goalsService.findGoal({ id });
+    if (!goal) {
+      throw new NotFoundException(id);
     }
 
-    @Query(returns => Goal)
-    async findGoal(@Args('id') id: number): Promise<Goal> {
-        const goal = await this.goalsService.findGoal({ id});
-        if(!goal) {
-            throw new NotFoundException(id);
-        }
+    return goal;
+  }
 
-        return goal;
+  @Mutation((returns) => Goal)
+  async newGoal(
+    @Args('text') text: string,
+    @Args('completed') completed: boolean,
+  ): Promise<Goal> {
+    const goal = await this.goalsService.createGoal({ text, completed });
+
+    if (!goal) {
+      throw new ConflictException(text);
     }
 
-    @Mutation(returns => Goal)
-    async newGoal(@Args('text') text: string, @Args('completed') completed: boolean ): Promise<Goal> {
-        const goal = await this.goalsService.createGoal({ text, completed });
+    return goal;
+  }
 
-        if(!goal) {
-            throw new ConflictException(text);
-        }
+  @Mutation((returns) => Goal)
+  async editGoal(
+    @Args('id') id: number,
+    @Args('text') text: string,
+    @Args('completed') completed: boolean,
+  ): Promise<Goal> {
+    const goal = await this.goalsService.editGoal({
+      where: { id },
+      data: { text, completed },
+    });
 
-        return goal;
+    if (!goal) {
+      throw new ConflictException(text);
     }
 
-    @Mutation(returns => Goal)
-    async editGoal(
-        @Args('id') id: number,
-        @Args('text') text: string,
-        @Args('completed') completed: boolean): Promise<Goal> {
-            
-            const goal = await this.goalsService.editGoal({ where: { id }, data: { text, completed }});
+    return goal;
+  }
 
-            if(!goal) {
-                throw new ConflictException(text);
-            }
+  @Mutation((returns) => Goal)
+  async deleteGoal(@Args('id') id: number): Promise<Goal> {
+    const goal = await this.goalsService.deleteGoal({ id });
 
-            return goal;
+    if (!goal) {
+      throw new NotFoundException(id);
     }
 
-    @Mutation(returns => Goal)
-    async deleteGoal(@Args('id') id: number): Promise<Goal> {
-        const goal = await this.goalsService.deleteGoal({ id });
-
-        if(!goal) {
-            throw new NotFoundException(id)
-        }
-
-        return goal;
-    }
+    return goal;
+  }
 }
