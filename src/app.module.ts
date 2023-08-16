833a052fcc8140ba5a7479bf37a7c084edc29459
Maria Lucia Solero <marialucia.solero@gmail.com>
2023-08-16 02:29:32 +0100

    fix: ci problem

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,16 +2,17 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { GoalsModule } from './goals/goals.module';
+import { HealthcheckController } from './healthcheck/healthcheck.controller';
 
 @Module({
   imports: [
     GoalsModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: 'schema.gql'
-    })
+      autoSchemaFile: 'schema.gql',
+    }),
   ],
-  controllers: [],
+  controllers: [HealthcheckController],
   providers: [],
 })
 export class AppModule {}
